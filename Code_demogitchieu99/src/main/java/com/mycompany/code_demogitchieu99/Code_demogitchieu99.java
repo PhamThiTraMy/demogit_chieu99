98888a6a25dd4e374f6e95695c9404c6a086c2d5
PhamThiTraMy <tramy06spt@gmail.com>
2025-09-09 15:32:20 +0700

    Khoi tao du an

---
+++ Code_demogitchieu99/src/main/java/com/mycompany/code_demogitchieu99/Code_demogitchieu99.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.code_demogitchieu99;
+
+/**
+ *
+ * @author TraMy
+ */
+public class Code_demogitchieu99 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tra My!");
+    }
+}
