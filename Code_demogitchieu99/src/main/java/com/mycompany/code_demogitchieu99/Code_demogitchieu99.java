c36f51944b09f80ba8af58c2bebb4fed416d8899
PhamThiTraMy <tramy06spt@gmail.com>
2025-09-09 15:38:22 +0700

    them chuc nang moi

--- Code_demogitchieu99/src/main/java/com/mycompany/code_demogitchieu99/Code_demogitchieu99.java
+++ Code_demogitchieu99/src/main/java/com/mycompany/code_demogitchieu99/Code_demogitchieu99.java
@@ -12,5 +12,6 @@ public class Code_demogitchieu99 {
 
     public static void main(String[] args) {
         System.out.println("Hello Tra My!");
+        System.out.println(" Day thuc hanh java");
     }
 }
